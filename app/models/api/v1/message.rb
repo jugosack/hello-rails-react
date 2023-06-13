aea85d08fcd93d475859f0f52b10044b2bd0475b
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-13 22:10:23 +0200

    Add scaffold structure in rails app

---
+++ app/models/api/v1/message.rb
@@ -0,0 +1,2 @@
+class Api::V1::Message < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
   # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

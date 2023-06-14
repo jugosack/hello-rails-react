97899f6d3c7e08ff83a1ad922382aa264f91c4f0
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-14 14:19:34 +0200

    Fixing rubocop errors

--- test/controllers/api/v1/messages_controller_test.rb
+++ test/controllers/api/v1/messages_controller_test.rb
@@ -1,45 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::MessagesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @api_v1_message = api_v1_messages(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get api_v1_messages_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_api_v1_message_url
     assert_response :success
   end
 
-  test "should create api_v1_message" do
-    assert_difference("Api::V1::Message.count") do
+  test 'should create api_v1_message' do
+    assert_difference('Api::V1::Message.count') do
       post api_v1_messages_url, params: { api_v1_message: { text: @api_v1_message.text } }
     end
 
     assert_redirected_to api_v1_message_url(Api::V1::Message.last)
   end
 
-  test "should show api_v1_message" do
+  test 'should show api_v1_message' do
     get api_v1_message_url(@api_v1_message)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_api_v1_message_url(@api_v1_message)
     assert_response :success
   end
 
-  test "should update api_v1_message" do
+  test 'should update api_v1_message' do
     patch api_v1_message_url(@api_v1_message), params: { api_v1_message: { text: @api_v1_message.text } }
     assert_redirected_to api_v1_message_url(@api_v1_message)
   end
 
-  test "should destroy api_v1_message" do
-    assert_difference("Api::V1::Message.count", -1) do
+  test 'should destroy api_v1_message' do
+    assert_difference('Api::V1::Message.count', -1) do
       delete api_v1_message_url(@api_v1_message)
     end
 
